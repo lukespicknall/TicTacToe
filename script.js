d1fd1831188c9bed4f4b8102cf47c37ddba32ee2
Luke Spicknall <lukejspicknall@gmail.com>
2023-04-25 22:42:38 -0400

    Begin gameboard structure

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let gameBox = document.querySelectorAll('.game-box');
+const gameBox = document.getElementById('game-box');
+// function ipsy() {
+//   gameBox.style.border = '5px solid red';
+// }
+// ipsy();
 
-const gameBoard = ["", "", "", "", "", "", "", "", ""];
+// const gameBoard = ["", "", "", "", "", "", "", "", ""];
 
-console.log(gameBoard)
+// console.log(gameBoard)
 
-// const createBoard = (function () { 
-//   const gameBoard = ['', '', '', '', '', '', '', '', '']
-//   gameBox.appendChild(gameBoard);
+const createBoard = (() => { 
+  const gameBoard = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+  gameBox.style.border = "5px solid red";
+//   gameBox.innerHTML(gameBoard);
 //   return { gameDisplay };
-// })();
+// console.log(gameBoard);
+})();
