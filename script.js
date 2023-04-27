0aa39e96166860df8f9315425a27df226148b2bc
Luke Spicknall <lukejspicknall@gmail.com>
2023-04-26 22:21:24 -0400

    make js generate the gameBoard grid and fill its cell's with the content of markContainer[];

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 const gameBox = document.querySelector('#game-box');
-// function ipsy() {
-//   gameBox.style.border = '5px solid red';
-// }
-// ipsy();
 
-// const gameBoard = ["", "", "", "", "", "", "", "", ""];
-
-// console.log(gameBoard)
-
-const createBoard = (() => { 
-  const gameBoard = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
-  for (let i = 0; i < gameBoard.length; i++) {
-    const spot = document.createElement('div');
-    spot.textContent(gameBoard[i]);
-    gameBox.appendChild(spot);
-    // console.log('hi')
+const createBoard = (() => {
+  const markContainer = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+  const gameBoard = document.createElement('div');
+  gameBoard.className = 'game-board';
+  for (let i = 0; i < markContainer.length; i += 1) {
+    const square = document.createElement('div');
+    square.id = i;
+    square.className = 'game-square';
+    square.textContent = markContainer[i];
+    gameBoard.appendChild(square);
   }
-  gameBox.style.border = "5px solid red";
-//   gameBox.innerHTML(gameBoard);
-//   return { gameDisplay };
-// console.log(gameBoard);
+  gameBox.appendChild(gameBoard);
+  gameBox.style.border = 'solid 5px red';
+//   return { markContainer, gameBoard };
 })();
