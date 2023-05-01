8eaad7a0613a1b0b7474128a2ca6ed30836f8f26
Luke Spicknall <lukejspicknall@gmail.com>
2023-04-30 20:23:35 -0400

    add reset function to clear markBox array. linked to HTML button.

--- script.js
+++ script.js
@@ -9,19 +9,39 @@ const gameboard = (() => {
   const square = document.querySelectorAll('.square');
   const markBox = ['x', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'];
 
-  const setBoard = (() => {
+  // loops over markBox and displays each index on each game square
+  const setBoard = () => {
     for (let i = 0; i < markBox.length; i += 1) {
       square[i].textContent = markBox[i];
     }
-  });
+  };
 
   // use this later to update markBox with playerMarks
-  const setMark = ((squareId, playerMark) => {
+  const setMark = (squareId, playerMark) => {
+    if (squareId >= '9') return;
     markBox[squareId] = playerMark;
     setBoard();
-  });
-  return { setBoard, setMark, markBox };
+  };
+
+  // can be called to clear board
+  const reset = (() => {
+    const resetButton = document.querySelector('.resetBtn');
+    resetButton.addEventListener('click', () => {
+      for (let i = 0; i < markBox.length; i += 1) {
+        markBox[i] = '';
+      }
+      setBoard();
+    });
+  })();
+
+  return { setBoard, setMark, reset };
 })();
+
+const game = () => {
+  const playerX = Player('X', 0);
+  const playerO = Player('O', 0);
+  let turn = 1
+};
 
 // const Player = (mark) => {
 
