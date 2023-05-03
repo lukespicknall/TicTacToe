a98cfa6e164f802220285629abf32f07bc5ef4a3
Luke Spicknall <lukejspicknall@gmail.com>
2023-05-02 23:37:41 -0400

    create another option for getting each player mark instance's index into new array to check them for winning combos. here (in the game module) as the square is clicked within play(), the clicked square's ID (which is equivalent to it's index position) immediately gets pushed into an xSpots or oSpots array based on who the current player is. those arrays are then made available.

--- script.js
+++ script.js
@@ -38,18 +38,21 @@ const gameBoard = (() => {
 
   // checks markBox for mark and creates new array with each mark instance's index
   // frankly i dont quite understand this array manipulation . . .
-  const checkWinner = () => {
-    const xChecker = markBox.map((mark, idx) => (mark === 'X' ? idx : '')).filter(String);
-    console.log(xChecker);
-    const oChecker = markBox.map((mark, idx) => (mark === 'O' ? idx : '')).filter(String);
-    console.log(oChecker);
-    // const checker = markBox.filter((mark) => mark.includes('X'));
-    // console.log(checker);
-    // return { checker };
-  };
+
+  // const checkWinner = () => {
+  //   const xChecker = markBox.map((mark, idx) => (mark === 'X' ? idx : '')).filter(String);
+  //   console.log(xChecker);
+  //   const oChecker = markBox.map((mark, idx) => (mark === 'O' ? idx : '')).filter(String);
+  //   console.log(oChecker);
+  // another approach . . . .
+  // const checker = markBox.filter((mark) => mark.includes('X'));
+  // console.log(checker);
+  // return { checker };
+  // };
 
   return {
-    setBoard, setMark, reset, turn, checkWinner,
+    setBoard, setMark, reset, turn,
+    // checkWinner,
   };
 })();
 
@@ -70,6 +73,8 @@ const game = (() => {
   // listen for click on each game square and adds
   // playerMark to square associated with squareID. advances turn count
   const play = (() => {
+    const xSpots = [];
+    const oSpots = [];
     const square = document.querySelectorAll('.square');
     square.forEach((item) => {
       item.addEventListener('click', (e) => {
@@ -78,9 +83,15 @@ const game = (() => {
         const squareId = e.target.id;
         const playerMark = getPlayerTurn().currentPlayer.getMark();
         gameBoard.setMark(squareId, playerMark);
-        gameBoard.checkWinner();
+        // gameBoard.checkWinner();
+
+        // another way to get index #s into arrays to check for winner.
+        // use this or gameboard checkWinner function
+        if (playerMark === 'X') xSpots.push(parseInt(squareId, 10)); // the 10 is radix# and just means base 10
+        if (playerMark === 'O') oSpots.push(parseInt(squareId, 10));
       });
     });
+    return { xSpots, oSpots };
   })();
 
   return { getPlayerTurn, play };
